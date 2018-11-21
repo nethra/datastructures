8ed7b66d05a35f9e5c88692b20d6b9cbbae05628
Nethra Madeshappa <nethra.madesh@gmail.com>
2018-11-21 16:12:40 +0530

    LRU cache implementation

--- src/main/java/com/practice/Miscellaneous/LRUCache.java
+++ src/main/java/com/practice/Miscellaneous/LRUCache.java
@@ -1,4 +1,59 @@
 package com.practice.Miscellaneous;
 
-public class LRUCache {
+
+import java.util.HashMap;
+
+class Node {
+    int key;
+    int value;
+    Node prev;
+    Node next;
+
+    Node(int key, int value) {
+        this.key = key;
+        this.value = value;
+        prev = null;
+        next = null;
+
+    }
 }
+
+public class LRUCache {
+    Node head;
+    Node end;
+
+    HashMap<Integer, Node> hashMap = new HashMap<Integer, Node>();
+
+    public void set(int key, int value) {
+
+    }
+
+    public int get(int key) {
+
+        if (hashMap.containsKey(key))
+
+        {
+            Node n = hashMap.get(key);
+            remove(n);
+            setHead(n);
+            return n.value;
+        }
+
+        return -1;
+    }
+
+    public void remove(Node n) {
+
+
+
+    }
+
+    public void setHead(Node n) {
+
+
+
+
+
+    }
+}
+
